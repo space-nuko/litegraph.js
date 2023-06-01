2b14fb0df2a5fbe2012587fac728b239c2637a07
space-nuko <24979496+space-nuko@users.noreply.github.com>
2023-06-01 15:43:32 -0500

    Prevent cloning subgraphs yet (inner nodes will not have unique IDs)

--- src/nodes/base.js
+++ src/nodes/base.js
@@ -24,6 +24,10 @@
         this.size = [140, 80];
         this.properties = { enabled: true };
         this.enabled = true;
+		// TEMP: Until ID reassignment is implemented
+		if (LiteGraph.use_uuids) {
+			this.clonable = false;
+		}
 
         //create inner graph
         this.subgraph = new LiteGraph.LGraph();
